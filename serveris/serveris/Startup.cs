41d11728cba5a716c25574eba443a78542d11711
edvinysz <edvinas.kavaliauskas@jkm.lt>
2018-11-11 19:06:15 +0200

    serveris - web UI
    web ui for CRUD

--- serveris/serveris/Startup.cs
+++ serveris/serveris/Startup.cs
@@ -33,11 +33,8 @@ namespace serveris
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
             app.UseMvc();
         }
     }
