1c5f0704de977bdd6c62b9de85222786ccd01704
Edvinas <edvinas.kavaliauskas@jkm.lt>
2018-11-13 18:57:17 +0200

    klases

---
+++ serveris/serveris/Models/BlockInGame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace serveris.Models
+{
+    public class BlockInGame
+    {
+        public long Id { get; set; }
+        public long BlockId { get; set; }
+        public bool IsFinish { get; set; }
+        public bool GiveDamage { get; set; }
+        public int PositionX { get; set; }
+        public int PositionY { get; set; }
+    }
+}
---
+++ serveris/serveris/Models/GameInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace serveris.Models
+{
+    public class GameInfo
+    {
+        public long Id { get; set; }
+        public string GameTitle { get; set; }
+        public long MapId { get; set; }
+        public long Player1Id { get; set; }
+        public long Player2Id { get; set; }
+        public long Player3Id { get; set; }
+        public long Player4Id { get; set; }
+        public int ConnectedPlayersCount { get; set; }
+        public long ResultTableId { get; set; }
+        public bool HasStarted { get; set; }
+        public bool HasEnded { get; set; }
+    }
+}
